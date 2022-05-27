e2aad5ef79aef3bd06f3ddb0a9ff8b8e699206b1
Graceugo <104877726+Graceugo@users.noreply.github.com>
2022-05-27 10:43:25 +0100

    Update main.py

--- starter-files/Finding-Anagrams/main.py
+++ starter-files/Finding-Anagrams/main.py
@@ -6,6 +6,14 @@
 
 def find_anagram(word, anagram):
     # [assignment] Add your code here
+  str1 = "smile"
+  str2 = "miles"
+  str1_word = sorted(str1)
+  str2_anagram = sorted(str2)
 
-    return True
+  if str_word == str_anagram:
+     return True
+  else :
+     return False
+print(find_anagram("smile", "miles"))
 
