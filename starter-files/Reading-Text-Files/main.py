910c034798c3c052846732601c29fd4d460c60d1
Graceugo <104877726+Graceugo@users.noreply.github.com>
2022-05-27 20:49:49 +0100

    Update main.py

--- starter-files/Reading-Text-Files/main.py
+++ starter-files/Reading-Text-Files/main.py
@@ -3,14 +3,27 @@
 # count_words("The cake is done. It is a big cake!") 
 # --> {"cake":2, "big":1, "is":2, "the":1, "a":1, "it":1}
 
-def read_file_content(filename):
+def read_file_content(file name):
     # [assignment] Add your code here 
-    
+    word = story.txt.split()
+    result = len(word)
+    Print(result)
     return "Hello World"
 
 
 def count_words():
     text = read_file_content("./story.txt")
     # [assignment] Add your code here
+    words = text.split()
+    print (words)
+    count = dict
 
+    For word in words:
+        if word in count.keys():
+           count[word] += 1
+        else:
+           count[word] =1
+
+print(count_words)
+     
     return {"as": 10, "would": 20}
